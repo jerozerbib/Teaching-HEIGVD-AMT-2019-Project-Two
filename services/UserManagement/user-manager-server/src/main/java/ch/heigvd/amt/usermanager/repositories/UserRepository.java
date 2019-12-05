a322492e461047c3322944e084d2cf18fb23f41b
Adam <adam.zouari@heig-vd.ch>
2019-12-05 17:14:33 +0100

    remove user manager files

--- services/UserManagement/user-manager-server/src/main/java/ch/heigvd/amt/usermanager/repositories/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package ch.heigvd.amt.usermanager.repositories;
-
-import ch.heigvd.amt.usermanager.entities.UserEntity;
-import org.springframework.data.repository.CrudRepository;
-
-
-public interface UserRepository extends CrudRepository<UserEntity, Long>{
-
-}
