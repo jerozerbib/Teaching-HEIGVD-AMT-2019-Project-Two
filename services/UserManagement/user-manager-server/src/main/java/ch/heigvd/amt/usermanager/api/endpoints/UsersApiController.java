7ad37e494d7eaefb3132d364e5718b9354367c87
jerozerbib <jerozerbib@gmail.com>
2019-12-13 16:11:40 +0100

    Almost done fixing up specs but without testing it

--- services/UserManagement/user-manager-server/src/main/java/ch/heigvd/amt/usermanager/api/endpoints/UsersApiController.java
+++ services/UserManagement/user-manager-server/src/main/java/ch/heigvd/amt/usermanager/api/endpoints/UsersApiController.java
@@ -1,5 +1,7 @@
 package ch.heigvd.amt.usermanager.api.endpoints;
 
+import ch.heigvd.amt.usermanager.api.model.UserInput;
+import ch.heigvd.amt.usermanager.api.model.UserOutput;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import ch.heigvd.amt.usermanager.api.UsersApi;
 import ch.heigvd.amt.usermanager.api.model.User;
@@ -14,7 +16,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 import java.net.URI;
 import java.util.ArrayList;
@@ -29,7 +30,12 @@ public class UsersApiController implements UsersApi {
     @Autowired
     UserRepository userRepository;
 
-    public ResponseEntity<Object> createUser(@ApiParam(value = "", required = true) @Valid @RequestBody User user) {
+    /**
+     * Creates a new User from a UserInput using the request method POST
+     * @param user UserInput
+     * @return a new Object
+     */
+    public ResponseEntity<Object> createUser(@ApiParam(value = "", required = true) @Valid @RequestBody UserInput user) {
         UserEntity newUserEntity = toUserEntity(user);
         userRepository.save(newUserEntity);
         String email = newUserEntity.getEmail();
@@ -41,36 +47,44 @@ public class UsersApiController implements UsersApi {
         return ResponseEntity.created(location).build();
     }
 
-    private UserEntity toUserEntity(@Valid User user) {
+    /**
+     * Converts a UserInput into a UserEntity
+     * @param user to convert
+     * @return a UserEntity
+     */
+    private UserEntity toUserEntity(@Valid UserInput user) {
         UserEntity entity = new UserEntity();
+        entity.setEmail(user.getEmail());
         entity.setFirstName(user.getFirstName());
         entity.setLastName(user.getLastName());
-        entity.setPassword(user.getPassword()); // TODO Check si le password doit etre la..
+        entity.setPassword(user.getPassword());
+        entity.setSalt(user.getSalt());
         return entity;
     }
 
-    public ResponseEntity<List<User>> getUsers() {List<User> users = new ArrayList<>();
+    /**
+     * Gets all the Users from the database using the request method GET
+     * @return a List of UserOutputs
+     */
+    public ResponseEntity<List<UserOutput>> getUsers() {
+        List<UserOutput> users = new ArrayList<>();
         for (UserEntity userEntity : userRepository.findAll()) {
             users.add(toUser(userEntity));
         }
         //TODO handle pagination
-        /*
-        User staticUser = new User();
-        staticUser.setColour("red");
-        staticUser.setKind("banana");
-        staticUser.setSize("medium");
-        List<User> Users = new ArrayList<>();
-        Users.add(staticUser);
-        */
         return ResponseEntity.ok(users);
     }
 
-    private User toUser(UserEntity entity) {
-        User user = new User();
+    /**
+     * Converts a UserEntity in a UserOutput
+     * @param entity to convert
+     * @return a UserOutput
+     */
+    private UserOutput toUser(UserEntity entity) {
+        UserOutput user = new UserOutput();
         user.setEmail(entity.getEmail());
         user.setFirstName(entity.getFirstName());
         user.setLastName(entity.getLastName());
-        user.setPassword(entity.getPassword()); // TODO Check si le password doit etre la..
         return user;
     }
 
--- services/UserManagement/user-manager-server/src/main/java/ch/heigvd/amt/usermanager/entities/UserEntity.java
+++ services/UserManagement/user-manager-server/src/main/java/ch/heigvd/amt/usermanager/entities/UserEntity.java
@@ -1,11 +1,18 @@
 package ch.heigvd.amt.usermanager.entities;
 
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import java.io.Serializable;
 
+@Setter
+@Getter
+@EqualsAndHashCode
 @Entity
 public class UserEntity implements Serializable {
 
@@ -15,33 +22,5 @@ public class UserEntity implements Serializable {
     private String firstName;
     private String lastName;
     private String password;
-
-    public String getEmail() {
-        return email;
-    }
-
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
+    private String salt;
 }
