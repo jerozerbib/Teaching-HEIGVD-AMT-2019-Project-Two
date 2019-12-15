123710333f01c224dbf185440b9d800e45e96e49
Adam <adam.zouari@heig-vd.ch>
2019-12-15 10:42:04 +0100

    add docker application properties and persistence db on volume

---
+++ docker-images/user-manager-server/build-docker-image.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+mkdir tmp
+
+mvn clean package -f ../../services/UserManagement/user-manager-server/pom.xml
+cp ../../services/UserManagement/user-manager-server/target/user-manager-server*.jar ./tmp/
+
+docker build -t user-manager/user-manager-server .
